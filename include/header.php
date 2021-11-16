5a50fbdd22e3b7098c3e32c5ec7adc050921b763
dayalubajpai <dayalubajpai@gmail.com>
2021-11-17 01:35:36 +0530

    avk

---
+++ include/header.php
@@ -0,0 +1,86 @@
+<html>
+
+<head>
+    <title>Social Crawler - Best Digital Agency in Lucknow</title>
+    <!-- Bootstrap Cdn Start -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap Cdn End -->
+    <!-- Css include -->
+    <style>
+        :root {
+            --primary-color: #5E4F94;
+            --secondary-color: black;
+        }
+
+        .nav-item {
+            text-align: center;
+            color: var(--primary-color);
+        }
+
+        .navbar-light .navbar-nav .nav-link {
+            color: var(--primary-color);
+        }
+
+        .navbar-light .navbar-nav .nav-link:hover {
+            color: var(--secondary-color);
+        }
+
+        .mobile-menu-icon {
+            color: var(--primary-color);
+        }
+
+        .navbar-light .navbar-toggler {
+            color: var(--primary-color);
+            border-color: transparent;
+        }
+
+        @media only screen and (max-width: 600px) {
+            .nav-link {
+                border-bottom: 1px solid;
+            }
+        }
+    </style>
+    <!-- Css Include End -->
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a href="file:///C:/xampp/htdocs/socialcrawler/include/header.html" class="navbar-brand"><img src="img\final logo white bg.png" alt="" height="60"></a>
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#mynavbar">
+                    <span class="mobile-menu-icon">
+                        <i class="fa fa-sliders" aria-hidden="true"></i>
+                    </span>
+                </button>
+                <div class="collapse navbar-collapse" id="mynavbar">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item"><a href="#" class="nav-link">
+                                <img src="img\home trace.svg" alt="" height="50">
+                            </a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">
+                                <img src="img\social crawler about us.svg" alt="" height="50">
+                            </a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">
+                                <img src="img\team sc.svg" alt="" height="50">
+                            </a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">
+                                <img src="img\work sc.svg" alt="" height="50">
+                            </a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">
+                                <img src="img\contact sc.svg" alt="" height="50"> 
+                            </a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+</body>
+
+</html>
